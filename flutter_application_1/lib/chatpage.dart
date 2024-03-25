76b4ddda7b7c53fddd70aabbd1077cd0373a5f15
Anna <aelise17264@gmail.com>
2024-03-25 11:22:41 -0600

    added mock message json to be called dynamically by chat page

--- flutter_application_1/lib/chatpage.dart
+++ flutter_application_1/lib/chatpage.dart
@@ -1,39 +1,47 @@
 // ignore_for_file: prefer_const_constructors, sort_child_properties_last, prefer_const_literals_to_create_immutables, use_super_parameters, prefer_final_fields
 
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_application_1/models/chatmessage.dart';
 import 'package:flutter_application_1/widgets/chatbubble.dart';
 import 'package:flutter_application_1/widgets/chatinput.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   ChatPage({Key? key}) : super(key: key);
-  final imageAddress = Image.asset('resource/catnap.jpg');
 
-  List<ChatMessage> _messages = [
-    ChatMessage(
-      text: "First Message",
-      id: '1',
-      createdAt: 2131231242,
-      author: Author(userName: 'opal1'),
-    ),
-    ChatMessage(
-      text: "Second Message",
-      id: '1',
-      createdAt: 2131231242,
-      author: Author(userName: 'fred2'),
-    ),
-    ChatMessage(
-      text: "Third Message",
-      id: '1',
-      createdAt: 2131231242,
-      author: Author(userName: 'opal1'),
-    ),
-  ];
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  List<ChatMessage> _messages = [];
+  _loadInitialMessages() async {
+    final response =
+        await rootBundle.loadString('resources/mock_messages.json');
+
+    final List<dynamic> decodedList = jsonDecode(response) as List;
+
+    final List<ChatMessage> _chatMessages = decodedList.map((listItem) {
+      return ChatMessage.fromJson(listItem);
+    }).toList();
+
+    setState(() {
+      _messages = _chatMessages;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _loadInitialMessages();
+    super.initState();
+  }
 
   // get onPressed => null;
-  // final String username;
   @override
   Widget build(BuildContext context) {
     final username = ModalRoute.of(context)!.settings.arguments as String;
@@ -60,7 +68,7 @@ class ChatPage extends StatelessWidget {
                   itemCount: _messages.length,
                   itemBuilder: (context, index) {
                     return ChatBubble(
-                        alignment: index % 2 == 0
+                        alignment: _messages[index].author.userName == 'opal1'
                             ? Alignment.centerLeft
                             : Alignment.centerRight,
                         message: _messages[index]);
--- flutter_application_1/lib/models/chatmessage.dart
+++ flutter_application_1/lib/models/chatmessage.dart
@@ -13,9 +13,21 @@ class ChatMessage {
       required this.createdAt,
       this.imageUrl,
       required this.author});
+
+  factory ChatMessage.fromJson(Map<String, dynamic> json) {
+    return ChatMessage(
+        text: json['text'],
+        id: json['id'],
+        createdAt: json['createdAt'],
+        author: Author.fromJson(json['author']));
+  }
 }
 
 class Author {
   String userName;
   Author({required this.userName});
+
+  factory Author.fromJson(Map<String, dynamic> json) {
+    return Author(userName: json['username']);
+  }
 }
